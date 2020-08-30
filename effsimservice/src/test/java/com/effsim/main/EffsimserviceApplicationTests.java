86d24e0c0b87a5fc7d3b7f788e72f0fd14b2a973
JavaShi <isitsqh@163.com>
2020-08-30 17:47:31 +0800

    Spring 测试类

---
+++ effsimservice/src/test/java/com/effsim/main/EffsimserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.effsim.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffsimserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+	
+
+}
